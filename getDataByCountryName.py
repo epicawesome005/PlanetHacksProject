cba0664b855e2af04090c91b96d848682d496148
vidhy <krish.balasp@gmail.com>
2020-05-24 12:19:41 -0700

    [5-24-2020] Updated with new suggestion list for country code/names

--- getDataByCountryName.py
+++ getDataByCountryName.py
@@ -7,16 +7,55 @@ while 1==1:
     if countryopt.upper()=='C' or countryopt.upper()=='N':
         break
 
+url = "https://covid-19-data.p.rapidapi.com/help/countries"
+
+querystring = {"format":"json"}
+
+headers = {
+    'x-rapidapi-host': "covid-19-data.p.rapidapi.com",
+    'x-rapidapi-key': "44efa3aff1msh3eea9d7a9e96681p1c8e0bjsn9f06ccff323e"
+    }
+
+response = requests.request("GET", url, headers=headers, params=querystring)
+
+countriesList_data = response.text
+getCountryListData = json.loads(countriesList_data)
+
+getCountryCodeList = []
+getCountryNameList = []
+
+for countryDtl in getCountryListData:
+    countryCode=countryDtl['alpha3code']
+    countryName=countryDtl['name']
+    getCountryCodeList.append(countryCode)
+    getCountryNameList.append(countryName.upper())
+
+
 if countryopt.upper() =="C":
-    jsonValue = str(input("What is the country code:"))
+    print("Choose Country Code from below:\n")
+    print(getCountryCodeList)
+    while 1==1:
+        jsonValue = str(input("What is the country code:"))
+
+        if jsonValue.upper() in getCountryCodeList:
+            break
+
     url = "https://covid-19-data.p.rapidapi.com/country/code"
     jsonKey = "code"
 
 
-elif countryopt.upper() =="N":
-    jsonValue = str(input("What is the country name:"))
+if countryopt.upper() =="N":
+    print("Choose Country Name from below:\n")
+    print("getCountryCodeName:", getCountryNameList)
+    while 1==1:
+        jsonValue = str(input("What is the country name:"))
+
+        if jsonValue.upper() in getCountryNameList:
+            break
+
     url = "https://covid-19-data.p.rapidapi.com/country"
     jsonKey = "name"
+
 
 querystring = {"format":"json",jsonKey:jsonValue}
 
@@ -74,18 +113,21 @@ else:
     print("Covid low impact area")
 
 if(covid_deaths > 5000):
-    print("Death rate is higher in this country")
+    print("Death rate is high in this country")
 else:
-    print("Death rate is lower in this country")
+    print("Death rate is low in this country")
 
-print("The percentage of the country's Covid-19 cases compared to the world's Covid-19 cases is" + " " + str(total_percent) + "% ")
+print("The percentage of the country's Covid-19 cases compared to the world's Covid-19 cases is" + " " + str(total_percent) + " % ")
 
-print("The percentage of the country's Covid-19 related deaths compared to the world's Covid-19 related deaths is" + " " + str(total_death) + "% ")
+print("The percentage of the country's Covid-19 related deaths compared to the world's Covid-19 related deaths is" + " " + str(total_death) + " % ")
 
-print("The percentage of the country's Covid-19 critical cases compared to the world's Covid-19 critical cases is" + " " + str(total_critic) + "% ")
+print("The percentage of the country's Covid-19 critical cases compared to the world's Covid-19 critical cases is" + " " + str(total_critic) + " % ")
 
-print("The percentage of the country's Covid-19 recovered cases compared to the world's Covid-19 recovered cases is" + " " + str(total_recover) + "% ")
+print("The percentage of the country's Covid-19 recovered cases compared to the world's Covid-19 recovered cases is" + " " + str(total_recover) + " % ")
 
-print("The recovery rate in this country is" + " " + str(recovery_rate) + "% ")
+print("The recovery rate in this country is" + " " + str(recovery_rate) + " % ")
 
-print("The death rate in this country is" + " " + str(death_rate) + "% ")
+print("The death rate in this country is" + " " + str(death_rate) + " % ")
+
+print("ALL DATA FROM RAPID API")
+
